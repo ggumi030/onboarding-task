35a4a9f0e65e0a9a3cac55e85877d0689a318a77
ggumi030 <tndus50843@gmail.com>
2024-11-10 04:34:28 +0900

    🎉 add : Exception
    1. Exception 처리 관련 코드 작성
    2. messages.properties 추가

--- src/main/java/com/spring/onboardingtask/global/exception/ExceptionResDto.java
+++ src/main/java/com/spring/onboardingtask/global/exception/ExceptionResDto.java
@@ -1,5 +1,14 @@
 package com.spring.onboardingtask.global.exception;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
 public class ExceptionResDto {
 
+    private HttpStatus statusCode;
+    private String message;
+    private String path;
 }
