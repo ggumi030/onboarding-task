71ec8fd3a46235bf4502ddbd1029860aa21e071d
ggumi030 <tndus50843@gmail.com>
2024-11-09 15:42:36 +0900

    init commit : 프로젝트 초기 세팅

---
+++ src/main/java/com/spring/onboardingtask/OnboardingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.spring.onboardingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/onboardingtask/OnboardingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.onboardingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
