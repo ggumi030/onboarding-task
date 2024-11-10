535754acee34980c0079f059b8741543b2637eea
ggumi030 <tndus50843@gmail.com>
2024-11-10 17:55:21 +0900

    🎉 add : 회원 가입 기능 구현
    1. 회원 가입 기능 구현
    2. 예외 처리 코드 작성 / messages.properties 추가

--- src/main/java/com/spring/onboardingtask/user/dto/SignupReqDto.java
+++ src/main/java/com/spring/onboardingtask/user/dto/SignupReqDto.java
@@ -1,5 +1,15 @@
 package com.spring.onboardingtask.user.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
 public class SignupReqDto {
 
+    @NotNull(message = "username 값이 필수로 들어있어야 합니다.")
+    private String username;
+    @NotNull(message = "password 값이 필수로 들어있어야 합니다.")
+    private String password;
+    @NotNull(message = "nickname 값이 필수로 들어있어야 합니다.")
+    private String nickname;
 }
--- src/main/java/com/spring/onboardingtask/user/dto/SignupResDto.java
+++ src/main/java/com/spring/onboardingtask/user/dto/SignupResDto.java
@@ -1,5 +1,14 @@
 package com.spring.onboardingtask.user.dto;
 
-public class SignupResDto {
+import com.spring.onboardingtask.user.entity.Authority;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
+public class SignupResDto {
+    private String username;
+    private String nickname;
+    private List<AuthDto> authorities;
 }
